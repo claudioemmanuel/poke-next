b4115a5fa1fa60d2e185bc5d8e473f306c346f74
Claudio Emmanuel <claudio.souza@linx.com.br>
2022-05-17 11:34:37 -0300

    add layout component

---
+++ pages/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span>PokeNext</span> &copy; 2022
+      </p>
+    </footer>
+  );
+};
---
+++ pages/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <Image
+          src={"/images/pokeball.png"}
+          width={30}
+          height={30}
+          alt="PokeNext"
+        ></Image>
+        <h1>PokeNext</h1>
+      </div>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>Sobre</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
